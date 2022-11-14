476f3039ed770fbe1219ae85991e1b444d624f76
pino-desktop <xmvziron>
2022-11-14 22:10:26 +0100

    Changed Emacs keybinding.

--- config.h
+++ config.h
@@ -63,13 +63,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run",/* "-m", dmenumon*/  "-fn", dmenufont, /* "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, */ NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *emacscmd[]  = { "emacsclient -c", NULL };
+static const char *emacscmd[]  = { "emacsclient", "-c", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,       	XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,       	XK_m, spawn,          {.v = emacscmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -101,6 +100,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	/* Custom keybindings */
+	{ MODKEY|ShiftMask,       	XK_m, spawn,          {.v = emacscmd } },
 };
 
 /* button definitions */
