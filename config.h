a5574858c8f0ca4eaf6832f0676749bd971df00b
ydalton <r0930164@student.thomasmore.be>
2022-12-05 22:34:36 +0000

    Changed fonts, gap size, color, browser command.

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 1;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;
 static const unsigned int systrayspacing = 2;
@@ -10,12 +10,12 @@ static const unsigned int systraypinningfailfirst = 1;
 static const unsigned int showsystray = 1;
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
-static const unsigned int snap	    = 31;		/* snap pixel */
-static const char *fonts[]          = { "Fira Code:size=10" };
-static const char dmenufont[]       = "Fira Code:size=10";
+static const unsigned int snap	    = 20;		/* snap pixel */
+static const char *fonts[]          = { "UbuntuMono Nerd Font:size=11" };
+static const char dmenufont[]       = "UbuntuMono Nerd Font:size=11";
 static const char col_gray1[]       = "#141622";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#777777";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#21577b";
 static const char *colors[][3]      = {
@@ -69,6 +69,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run",/* "-m", dmenumon*/  "-fn", dmenufont, /* "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, */ NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *emacscmd[]  = { "emacsclient", "-c", NULL };
+static const char *browscmd[]  = { "brave", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -107,6 +108,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	/* Custom keybindings */
 	{ MODKEY|ShiftMask,       	XK_m, spawn,          {.v = emacscmd } },
+	{ MODKEY|ShiftMask,       	XK_b, spawn,          {.v = browscmd } },
 };
 
 /* button definitions */
